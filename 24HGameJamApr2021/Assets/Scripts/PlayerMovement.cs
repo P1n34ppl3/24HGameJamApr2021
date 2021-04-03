c0a678d97e9ccd8a90aeddd7aa4f33511caad9da
P1n34ppl3 <lander.deveuster@telenet.be>
2021-04-04 00:04:57 +0200

    bug fix player movement

--- 24HGameJamApr2021/Assets/Scripts/PlayerMovement.cs
+++ 24HGameJamApr2021/Assets/Scripts/PlayerMovement.cs
@@ -22,13 +22,11 @@ public class PlayerMovement : MonoBehaviour
 
     void OnEnable()
     {
-        Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
     }
 
     void OnDisable()
     {
-        Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
     }
 
