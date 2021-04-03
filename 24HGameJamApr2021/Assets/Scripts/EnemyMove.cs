4252e2cb988410d004c0e8f9d49734835bdcb8e3
NewryBenson <76691448+NewryBenson@users.noreply.github.com>
2021-04-03 23:12:51 +0200

    samlpe

--- 24HGameJamApr2021/Assets/Scripts/EnemyMove.cs
+++ 24HGameJamApr2021/Assets/Scripts/EnemyMove.cs
@@ -7,10 +7,16 @@ public class EnemyMove : MonoBehaviour
     public float Speed = 10f;
     public float Spawnrate = 5f;
     public Transform target;
+    private Rigidbody rb;
 
     void Start()
     {
         Aim();
+    }
+
+    void Update()
+    {
+
     }
 
     void Aim()
@@ -20,5 +26,7 @@ public class EnemyMove : MonoBehaviour
         var rotation = Quaternion.LookRotation(lookPos);
         transform.rotation = Quaternion.Slerp(transform.rotation, rotation, 1);
     }
+    
+
     
 }
