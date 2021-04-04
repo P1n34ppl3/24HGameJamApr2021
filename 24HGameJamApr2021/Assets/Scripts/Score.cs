1d728de8e9bbfcde98dc0d02bee34514dd44bb22
NewryBenson <76691448+NewryBenson@users.noreply.github.com>
2021-04-04 13:14:18 +0200

    scripts

--- 24HGameJamApr2021/Assets/Scripts/Score.cs
+++ 24HGameJamApr2021/Assets/Scripts/Score.cs
@@ -2,28 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 
 public class Score : MonoBehaviour
 {
-    public static int CurrentScore = 0;
-    public static int HighScore = 0;
-    public GameObject highScoreText;
-    public GameObject scoreText;
+    public static int CurrentScore = 5;
+    public static int HighScore = 5;
+    public TextMeshProUGUI highScoreText;
+    public TextMeshProUGUI scoreText;
 
-    void Start()
-    {
 
-    }
-
+    
     void Update()
     {
+        
+        Debug.Log("check");
+        highScoreText.text = HighScore.ToString();
+        scoreText.text = CurrentScore.ToString();
         if (HighScore <= CurrentScore)
         {
             HighScore = CurrentScore;
         }
 
-        
+
     }
 }
-
