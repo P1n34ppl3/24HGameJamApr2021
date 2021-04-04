75e72ca0ef8dc24c1d24df3e6cbfc2e848fed095
NewryBenson <76691448+NewryBenson@users.noreply.github.com>
2021-04-04 15:36:49 +0200

    psuh

--- 24HGameJamApr2021/Assets/Scripts/StageMenager.cs
+++ 24HGameJamApr2021/Assets/Scripts/StageMenager.cs
@@ -29,23 +29,31 @@ public class StageMenager : MonoBehaviour
     }
     void Stage1()
     {
+        GameObject.FindObjectOfType<AudioManager>().Play("Horn");
         AnnouncText.text = "The enemies have breached the West gate, seek cover!";
         Invoke("clear", 3);
+        
     }
     void Stage2()
     {
+        GameObject.FindObjectOfType<AudioManager>().Play("Horn");
         AnnouncText.text = "The East gate has now been breached!";
         Invoke("clear", 3);
+      
     }
     void Stage3()
     {
+        GameObject.FindObjectOfType<AudioManager>().Play("Horn");
         AnnouncText.text = "The East gate has now been breached!";
         Invoke("clear", 3);
+        
     }
     void Stage4()
     {
+        GameObject.FindObjectOfType<AudioManager>().Play("Horn");
         AnnouncText.text = "The East gate has now been breached!";
         Invoke("clear", 3);
+       
     }
     void clear()
     {
