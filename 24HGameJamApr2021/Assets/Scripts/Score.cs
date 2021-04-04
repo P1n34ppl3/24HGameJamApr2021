5c3f1dabfc906d5208aad964bf7b99f8bd785410
P1n34ppl3 <lander.deveuster@telenet.be>
2021-04-04 12:03:07 +0200

    Update Score.cs

--- 24HGameJamApr2021/Assets/Scripts/Score.cs
+++ 24HGameJamApr2021/Assets/Scripts/Score.cs
@@ -3,12 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+
 public class Score : MonoBehaviour
 {
     public static int CurrentScore = 0;
     public static int HighScore = 0;
     public GameObject highScoreText;
     public GameObject scoreText;
+
+    void Start()
+    {
+
+    }
 
     void Update()
     {
